dc6609fd23d3dc2bed2c4ff61ebba1e0954aefe7
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-02-08 11:23:05 +0200

    Update Web_scrapper-images.py

--- Web_scrapper-images.py
+++ Web_scrapper-images.py
@@ -1,38 +1,49 @@
+import os
 import requests
 from bs4 import BeautifulSoup
-import os
 
-def scrape_images(url, save_dir):
-    # send a GET request to the URL
-    response = requests.get(url)
+# Create a list of the websites to scrape
+websites = ["https://stock.adobe.com/za/search/images?k=cats%20and%20dogs"]
 
-    # parse the HTML content of the page
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    # find all image tags on the page
-    images = soup.find_all("img")
-
-    # loop over each image
-    for i, image in enumerate(images):
-        # get the source URL of the image
-        src = image.get("src")
-
-        # send a GET request to download the image
-        response = requests.get(src)
-
-        # save the image to disk
-        with open(f"{save_dir}/image_{i}.jpg", "wb") as f:
-            f.write(response.content)
-
-# specify the URL of the page you want to scrape
-url = "https://www.example.com/images"
-
-# specify the directory where you want to save the images
+# Specify the directory to save the images
 save_dir = "images"
 
-# create the save directory if it doesn't exist
+# Specify the format to save the images in
+img_format = "png"
+
+# Make the save directory if it doesn't exist
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
-# call the scrape_images function
-scrape_images(url, save_dir)
+# Loop over each website
+for website in websites:
+    # Send a GET request to the website
+    response = requests.get(website)
+    
+    # If the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.content, "html.parser")
+        
+        # Find all the img tags
+        images = soup.find_all("img")
+        
+        # Loop over each image
+        for i, image in enumerate(images):
+            # Get the source URL of the image
+            src = image.get("src")
+            
+            # If the source URL is not None
+            if src is not None:
+                # Send a GET request to download the image
+                img_response = requests.get(src)
+                
+                # If the request was successful
+                if img_response.status_code == 200:
+                    # Save the image to disk
+                    with open(f"{save_dir}/image_{i}.{img_format}", "wb") as f:
+                        f.write(img_response.content)
+    # If the request was not successful
+    else:
+        # Print a message indicating that the request failed
+        print(f"Failed to fetch URL: {website} (status code {response.status_code})")
